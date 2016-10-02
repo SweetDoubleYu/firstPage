d9c9b42fea91b44bcccc29fb775f4c19fa9fb579
Rossign0l <artyom.rossignol@gmail.com>
2016-10-03 02:17:54 +0300

    Initial commit

---
+++ map.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?sid=WVCZ53AJhKUeSnIeYJuNLnEejMTHecyB&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;sourceType=constructor&amp;scroll=true"></script>
